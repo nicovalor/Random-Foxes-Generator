a11954a57d621b2379fc67ee0e1b4a76c64fe525
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-02 12:57:22 -0300

    more initial config in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -13,7 +8,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1>HELLO WORLD</h1>  
       </main>
     </>
