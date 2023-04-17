3a9eca62b6b3edddc0fbfbfb1d833fa7f8d57aaf
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-17 11:08:45 -0300

    Agrego boton para generar nuevo zorro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import RandomFox from '@/components/RandomFox'
 import { useState } from 'react'
+import type {MouseEventHandler} from 'react'
+
 
 const random = ()=> Math.floor(Math.random() * 123) + 1
 
@@ -8,6 +10,12 @@ type Item = {id: string, image: string}
 
 export default function Home() {
   const [images, setImages] = useState<Array<Item>>([])
+
+  const handleClick:MouseEventHandler<HTMLButtonElement> =(event)=>{
+    event.preventDefault()
+    const newItem: Item = {id: images.length.toString(), image: `https://randomfox.ca/images/${random()}.jpg`}
+    setImages([...images, newItem])
+  }
   return (
     <>
       <Head>
@@ -16,7 +24,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+        <button onClick={handleClick}>Add new fox</button>
+        {images.map((item,index) => <RandomFox key={index} image={item.image} />)}
       </main>
     </>
   )
