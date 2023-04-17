6ee2d758c4f58e9443cd6564bfb874a57554c7b2
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-17 14:32:49 -0300

    implemento lazy loading a las imagenes

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useRef, useState } from "react"
+
 type Props = {
     image: string
 }
 
 
 const RandomFox = ({image}: Props): JSX.Element => {
-    return <img src={image} alt='Random Fox'  width='500rem 'height='auto' />
+    const node = useRef<HTMLImageElement>(null)
+    const [src, setSrc] = useState<string>("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
+    useEffect(()=>{
+        //New observer
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach(entry => {
+                if(entry.isIntersecting){
+                    console.log('Hey!')
+                }
+            })
+        })
+
+        //Observe node
+        node.current && observer.observe(node.current)
+
+        //Disconect node
+        return ()=> observer.disconnect()
+    },
+    [])
+    return <img ref={node} src={src} alt='Random Fox'  width='500rem 'height='auto' className="rounded bg-grey-300" />
 }
 
 export default RandomFox
