76201f03b1ef7d23fa509709805db8995afb012a
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-17 10:20:16 -0300

    agrego tipo string al array  images del estado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import RandomFox from '@/components/RandomFox'
+import { useState } from 'react'
 
 const random = ()=> Math.floor(Math.random() * 123) + 1
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([])
   return (
     <>
       <Head>
