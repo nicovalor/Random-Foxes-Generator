db1d8aec8bcd52da80affae5cf08792852425cfc
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-19 19:07:52 -0300

    stiying

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,15 +23,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <main className='flex flex-col justify-center bg-cyan-950'>
-        <button onClick={handleClick} className='my-10 rounded-full bg-cyan-400'>Add new fox</button>
-        <div className='flex justify-center items-center'>
-          <div className='flex gap-4 w-4/5  bg-red-300'>
+      <main className='bg-sky-950'>
+        <div className='bg-blue-950 shadow flex justify-center items-center'>
+          <button onClick={handleClick} className='my-10 rounded-full bg-cyan-400 p-5'>Add new fox</button>
+        </div>
+        <div className='flex flex-col justify-center items-center '>
           {images.map((item,index) => { return <div key={index} className='w-96 h-fit'>
-              <LazyImage key={index} image={item.image} width='500rem 'height='auto' className="rounded bg-grey-300" /> 
+              <LazyImage key={index} image={item.image} width='500rem 'height='auto' className="rounded bg-grey-300 flex flex-col justify-center items-center my-4 shadow-xl" /> 
             </div>})}
           </div>
-        </div>
       </main>
     </>
   )
